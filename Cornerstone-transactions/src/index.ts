ad0c49b60a6237c83a489a054e92b0d0a85c047c
Drayton <Dr8ton@users.noreply.github.com>
2020-11-04 17:10:00 -0600

    Add files via upload

---
+++ Cornerstone-transactions/src/index.ts
@@ -0,0 +1,93 @@
+import puppeteer from 'puppeteer';
+import type { Student } from './secrets/students';
+const CREDENTIALS = require('../secrets/key.json');
+
+
+
+
+
+
+async function main() {
+    //nav
+    const browser = await puppeteer.launch({ headless: true });// slow down by 250ms 
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1000, height: 1000 });
+
+    let loginPage = await navToSite(transactionPageURL, page);
+    let transactionPage = await loginToCornerstone(loginPage)
+    for (const student of studentList) {
+
+
+        //search for studnet
+        const lastNameInputID = '#ctl00_ContentPlaceHolder1_tbLastName';
+        const firstNameInputID = '#ctl00_ContentPlaceHolder1_tbFirstName';
+        const searchButton = '#ctl00_ContentPlaceHolder1_btnSearch';
+
+        await page.waitForSelector('#ctl00_ContentPlaceHolder1_tbLastName')
+
+        await typeStudentName(transactionPage, lastNameInputID, student.lastName);
+        await typeStudentName(transactionPage, firstNameInputID, student.firstName);
+
+        await transactionPage.click(searchButton);
+
+        student.transactions = await scrapeTable(transactionPage);
+    }
+    // scrape transaction table and return purchases
+
+    printListOfTransactions(studentList)
+    browser.close();
+}
+
+
+function printListOfTransactions(list: Student[]) {
+    for (const student of list) {
+        console.log(`${student.lastName}, ${student.firstName} - ${student.transactions}`)
+    }
+
+}
+async function scrapeTable(page: puppeteer.Page): Promise<string[]> {
+
+
+    await page.waitForSelector('table.TransactionTable')
+    let transactionRows = await page.$$('table.TransactionTable > tbody > tr');
+    let transactions: string[] = [];
+    for (const row of transactionRows) {
+
+        let tds = await row.$$('td');
+        let amountPaid = await tds[5].evaluate(node => node.innerHTML.trim())
+        transactions.push(amountPaid)
+    }
+    return transactions;
+}
+
+async function typeStudentName(page: puppeteer.Page, selector: string, textToInput: string) {
+    const blank = await page.$(selector);
+    await blank!.click({ clickCount: 3 })
+    await blank!.type(textToInput);
+}
+
+async function navToSite(url: string | null, page: puppeteer.Page): Promise<puppeteer.Page> {
+    if (url) {
+        await page.goto(url);
+        return page;
+    } else {
+        page.goto('www.google.com');
+        return page;
+    }
+
+
+
+}
+
+async function loginToCornerstone(page: puppeteer.Page): Promise<puppeteer.Page> {
+    await page.waitForSelector('#userNameBox')
+    await page.type('#userNameBox', CREDENTIALS.cornerstone.login);
+    await page.type('#passWordBox', CREDENTIALS.cornerstone.password);
+    await page.click('#submit');
+    return page;
+}
+
+
+
+
+main();
